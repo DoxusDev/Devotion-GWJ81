a0cc7232a78a91d00d5ca8c3b2c9e3919e0e390a
Doxus <dsoler700@gmail.com>
2025-05-11 16:26:51 +0200

    First prototype, tilesets and assets

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,6 +1,7 @@
 extends Node
 
 @onready var camera_2d: Camera2D = $"../Camera2D"
+var camera_pos_y_increment = 650.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,13 +12,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
-	if body.name == "Player":  # o verifica con grupos
-		var cam = get_viewport().get_camera_2d()
-		if cam:
-			cam.global_position = target_camera_position
-
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+	print("he entrado en el area")
+	if body.name == "Player":
+		camera_2d.global_position.y -= camera_pos_y_increment
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2d
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -12,14 +13,23 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		#animated_sprite_2d.play("jump")
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+	
 	if direction:
+		animated_sprite_2d.play("run")
 		velocity.x = direction * SPEED
 	else:
+		animated_sprite_2d.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()

d9bef3cbab9550cb079b662b8033f64dddf87f6f
Doxus <dsoler700@gmail.com>
2025-05-14 18:32:56 +0200

    Music, wind sound, main menu, new platform and hud added

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,5 +1,9 @@
 extends Node
 
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
+@onready var music_sound: AudioStreamPlayer = $"../MusicSound"
+
+var in_second_zone = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	#animation_player.play("fade_in")
@@ -8,3 +12,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player" and not in_second_zone:
+		animation_player.play("intro_second_zone")
+		in_second_zone = true
---
+++ scripts/main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("accept"):
+		animation_player.play("fade_out")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_out":
+		get_tree().change_scene_to_file("res://scenes/intro.tscn")

d7788c40937b18ca9a45bbcd810ad12fa2c1926e
Doxus <dsoler700@gmail.com>
2025-05-16 20:15:45 +0200

    Really gold 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -50,7 +50,7 @@ func _physics_process(delta: float) -> void:
 		# Fuerza una pequeña caída para atravesar la plataforma
 		global_position.y += 2
 	
-	if Input.is_action_pressed("teleport") and is_on_floor():
+	if Input.is_action_pressed("teleport") and is_on_floor() and not last_flag_position == Vector2.ZERO:
 		global_position = last_flag_position
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.

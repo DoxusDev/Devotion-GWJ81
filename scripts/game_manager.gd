fa6bcae716cb6f2e9a59c8023f9c17cc7e1c7e67
Doxus <dsoler700@gmail.com>
2025-05-29 01:05:30 +0200

    New font, new feature map guide hud, changes in main menu

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -9,7 +9,25 @@ extends Node
 @onready var area_2d_intro_third_zone: Area2D = $"../Area2DIntroThirdZone"
 @onready var area_2d_intro_final_zone: Area2D = $"../Area2DIntroFinalZone"
 @onready var area_2d_intro_final_zone_2: Area2D = $"../Area2DIntroFinalZone2"
+@onready var down_arrow_key: Sprite2D = $"../PlatformDownTuto/DownArrowKey"
+@onready var s_key: Sprite2D = $"../PlatformDownTuto/SKey"
+@onready var down_joypad_btn: Sprite2D = $"../PlatformDownTuto/DownJoypadBtn"
+@onready var platform_down_tuto: Node2D = $"../PlatformDownTuto"
+@onready var move_tutorial: Node2D = $"../MoveTutorial"
+@onready var jump_tuto: Node2D = $"../JumpTuto"
+@onready var flag_tutorial: Node2D = $"../FlagTutorial"
+@onready var teleport_flag_tutorial: Node2D = $"../TeleportFlagTutorial"
+@onready var control_effects: AnimationPlayer = $"../ControlEffects"
+@onready var controller_joystick: Sprite2D = $"../MoveTutorial/ControllerJoystick"
+@onready var controller_direction_pad: Sprite2D = $"../MoveTutorial/ControllerDirectionPad"
+@onready var direction_pad_up_btn: Sprite2D = $"../JumpTuto/DirectionPadUpBtn"
+@onready var a_btn: Sprite2D = $"../JumpTuto/ABtn"
 @onready var left_arrow_key: Sprite2D = $"../MoveTutorial/LeftArrowKey"
+@onready var down_arrow_key_move: Sprite2D = $"../MoveTutorial/DownArrowKey"
+@onready var right_arrow_key: Sprite2D = $"../MoveTutorial/RightArrowKey"
+@onready var up_arrow_key: Sprite2D = $"../MoveTutorial/UpArrowKey"
+@onready var up_arrow_key_jump: Sprite2D = $"../JumpTuto/UpArrowKey"
+@onready var space_key: Sprite2D = $"../JumpTuto/SpaceKey"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -22,9 +40,35 @@ func _process(delta: float) -> void:
 
 func _input(event: InputEvent) -> void:
 	if Global.get_player_controller():
-		left_arrow_key.texture = preload("res://icon.svg")
+		down_arrow_key.visible = false
+		s_key.visible = false
+		left_arrow_key.visible = false
+		down_arrow_key_move.visible = false
+		right_arrow_key.visible = false
+		up_arrow_key.visible = false
+		up_arrow_key_jump.visible = false
+		space_key.visible = false
+		
+		controller_direction_pad.visible = true
+		controller_joystick.visible = true
+		down_joypad_btn.visible = true
+		direction_pad_up_btn.visible = true
+		a_btn.visible = true
 	else:
-		left_arrow_key.texture = preload("res://assets/UI/Keyboard Letters and Symbols.png")
+		down_arrow_key.visible = true
+		s_key.visible = true
+		left_arrow_key.visible = true
+		down_arrow_key_move.visible = true
+		right_arrow_key.visible = true
+		up_arrow_key.visible = true
+		up_arrow_key_jump.visible = true
+		space_key.visible = true
+		
+		controller_direction_pad.visible = false
+		controller_joystick.visible = false
+		down_joypad_btn.visible = false
+		direction_pad_up_btn.visible = false
+		a_btn.visible = false
 
 func _on_area_2d_intro_second_zone_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
@@ -47,6 +91,15 @@ func _on_dead_god_area_2d_body_entered(body: Node2D) -> void:
 		Engine.time_scale = 0.3
 		distortion_sound.play()
 		animation_player.play("ending")
+
+func _on_area_2d_quit_tutorial_ui_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		control_effects.stop()
+		platform_down_tuto.visible = false
+		move_tutorial.visible = false
+		jump_tuto.visible = false
+		flag_tutorial.visible = false
+		teleport_flag_tutorial.visible = false
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "outro_first_zone":
